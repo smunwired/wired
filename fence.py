6944351f9417d9902aef4bc4692df5e4ff865cd2
Stephan Munn <stephanmunn@hotmail.com>
2022-05-24 13:32:53 +0100

    modernised input and print commands, add db commit

--- fence.py
+++ fence.py
@@ -6,14 +6,14 @@ import time
 from datetime import datetime, timedelta
 
 con = mdb.connect('192.168.1.216', 'stef', 'pass', 'mydb')
-p0 = raw_input("enter mod, del or defaults to new : ") or "new"
-p1 = raw_input("enter date, y for yesterday, defaults to today : ") or time.strftime("%Y-%m-%d")
-p2 = raw_input("enter club, defaults to haverstock: ") or "haverstock"
-p3 = raw_input("enter text : (optional)") 
-p4 = raw_input("enter 1 for lesson, defaults to zero : ") or 0
-#p5 = raw_input("enter max : ")
-#p6 = raw_input("enter odo : ")
-#p7 = raw_input("enter notes : ")
+p0 = input("enter mod, del or defaults to new : ") or "new"
+p1 = input("enter date, y for yesterday, defaults to today : ") or time.strftime("%Y-%m-%d")
+p2 = input("enter club, defaults to haverstock: ") or "haverstock"
+p3 = input("enter text : (optional)") 
+p4 = input("enter 1 for lesson, defaults to zero : ") or 0
+#p5 = input("enter max : ")
+#p6 = input("enter odo : ")
+#p7 = input("enter notes : ")
 
 with con:
 
@@ -21,11 +21,11 @@ with con:
     if p0=="mod":
         cur.execute("update bike set tm=%s,dst=%s,av=%s,mx=%s,odo=%s,notes=%s where rdate=%s",
         (p2,p3,p4,p5,p6,p7,p1))
-        print "Number of rows updated:",  cur.rowcount
+        print("Number of rows updated:",  cur.rowcount)
     elif p0=="del":
         cur.execute("delete from bike  where rdate=%s",
         (p1))
-        print "Number of rows deleted:",  cur.rowcount
+        print("Number of rows deleted:",  cur.rowcount)
     else:
         if p1=="y":
             d = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
@@ -34,9 +34,10 @@ with con:
         sql="""insert into fence(dt,venue,txt,lesson) values (%s,%s,%s,%s)"""
         #cur.execute(sql, (d,))
         cur.execute(sql, (d,p2,p3,p4))
+        con.commit()
         #cur.execute("insert into fence (str) values (%s,);",
         #(d,))
-        print "Number of rows inserted:",  cur.rowcount
+        print("Number of rows inserted:",  cur.rowcount)
 
 #MariaDB [mydb]> desc fence;
 #+--------+-------------+------+-----+------------+----------------+
